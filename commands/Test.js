8c77e2a197c5253b0e075315a14e554147309da6
myles <mcawley38@googlemail.com>
2021-12-04 16:13:10 +0000

    - reverted Test command logic

--- commands/Test.js
+++ commands/Test.js
@@ -1,26 +1,6 @@
-const Command = require('./Command'),
-    colour = require('../helpers/colour'),
-    WhereQuery = require('../database/query/WhereQuery'),
-    query = new (require('../database/query/Query')),
-    createUniqueId = require('../helpers/createUniqueId'),
-    faker = require('faker')
+const Command = require('./Command')
 
 module.exports = class Test extends Command {
-    handle() {
-        colour.comment('Testing testing 123')
-
-        colour.comment('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-
-        let a  = query.setTable('users')
-            .create({
-                id: null,
-                uuid: createUniqueId('user'),
-                name: faker.name.findName(),
-                secret: faker.random.alpha(25),
-            })
-
-
-        console.log(a)
-    }
+	handle() {
+	}
 }
