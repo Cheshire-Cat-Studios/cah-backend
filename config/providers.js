b906439ee9c228cb806bce3c9e169de31e44bcb5
myles <mcawley38@googlemail.com>
2022-06-26 17:52:45 +0100

    - typo

--- config/providers.js
+++ config/providers.js
@@ -1,10 +1,12 @@
 const
+    PrototypeServiceProvider = require('../providers/PrototypeServiceProvider'),
     RouteServiceProvider = require('../providers/RouteServiceProvider'),
     AppServiceProvider = require('../providers/AppServiceProvider'),
     SocketServiceProvider = require('../providers/SocketServiceProvider'),
     EventServiceProvider = require('../providers/EventServiceProvider')
 
 module.exports = [
+    PrototypeServiceProvider,
     AppServiceProvider,
     RouteServiceProvider,
     SocketServiceProvider,
