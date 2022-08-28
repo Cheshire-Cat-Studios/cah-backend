ff4a3adc629d87932ba48c7280312d4da87a0e2f
myles <mcawley38@googlemail.com>
2022-08-28 14:06:12 +0100

    - cleaned up AppServiceProvider.js - abstracted router logic from RouteServiceProvider.js and into Router.js

---
+++ routes/Router.js
@@ -0,0 +1,62 @@
+module.exports = class Router {
+	route = null
+	app = null
+
+	setApp(app){
+		this.app = app
+
+		return this
+	}
+
+	setRoute(route){
+		this.route = route
+
+		return this
+	}
+
+	parseRoutes() {
+		this.route
+			.routes
+			.forEach(this.parseRoute, this)
+	}
+
+	parseRoute(route, parent) {
+		let self = this
+
+		route.method
+			? self.createExpressRoute(route, parent)
+			: route.routes.forEach(child_route => {
+
+				const new_parent = {
+					path: `${parent.path || ''}/${route.path || ''}`,
+					name: `${parent.path || ''}.${route.path || ''}`,
+					middleware: [
+						...(parent.middleware || []),
+						...route.middleware,
+					]
+				}
+
+				self.parseRoute(child_route, new_parent)
+			}, this)
+	}
+
+	//TODO: consider sacking off express routes, only using express for routing and sockets, node can do both adequately
+	createExpressRoute(route, parent) {
+		const url = `${parent.path}/${route.path}`.replace(/\/+/g, '/')
+
+		let route_lifecycle = [
+			...(parent.middleware || []),
+			...route.middleware,
+		]
+			.map(middleware => (req, res, next) => {
+				middleware.handle(req, res, next)
+			})
+
+		//TODO: consider wrapping below similar to above to allow for more complex controllers and middleware
+		route_lifecycle.push(route.method)
+
+		route.is_get
+			? this.app.get(url, route_lifecycle)
+			: this.app.post(url, route_lifecycle)
+	}
+}
